e40bb2395370a2f66be8509249d5e50f5d1798b6
LemonPy <134615071+lemonpyhub@users.noreply.github.com>
2025-09-23 15:57:53 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const currentUrl = encodeURIComponent(window.location.href);
-  const pageTitle = encodeURIComponent(document.title);
+// Google Analytics
+(function(){
+  var gtagScript = document.createElement('script');
+  gtagScript.async = true;
+  gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-Y88WHZECBL";
+  document.head.appendChild(gtagScript);
 
-  const shareLinks = {
-    facebook: `https://www.facebook.com/sharer/sharer.php?u=${currentUrl}`,
-    tiktok: `https://www.tiktok.com/share?url=${currentUrl}`,
-    whatsapp: `https://api.whatsapp.com/send?text=${pageTitle}%20${currentUrl}`,
-    telegram: `https://t.me/share/url?url=${currentUrl}&text=${pageTitle}`,
-    twitter: `https://twitter.com/intent/tweet?text=${pageTitle}&url=${currentUrl}`,
-    reddit: `https://www.reddit.com/submit?url=${currentUrl}&title=${pageTitle}`
-  };
-
-  document.querySelectorAll(".share-buttons a").forEach(button => {
-    button.addEventListener("click", function (e) {
-      e.preventDefault();
-
-      const platform = this.className;
-      const appUrl = this.getAttribute("href");
-      const fallbackUrl = shareLinks[platform];
-
-      // Cuba buka app
-      window.location.href = appUrl;
-
-      // Kalau gagal → fallback ke web
-      setTimeout(() => {
-        window.open(fallbackUrl, "_blank");
-      }, 800);
-    });
-  });
-});
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', 'G-Y88WHZECBL');
+})();
